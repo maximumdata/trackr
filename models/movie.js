bf00cacb9067d50169334d6d18fc6bd835496fb4
Mike Dettmer <mike@mikedettmer.com>
2016-03-19 22:51:55 -0400

    updating movie model

--- models/movie.js
+++ models/movie.js
@@ -2,12 +2,32 @@ var mongoose = require('../config/database');
 
 var Movie = mongoose.Schema({
   _owner: { type: String, ref: 'Account' },
-  title: String,
-  rating: Number,
-  review: String,
-  watchedInfo : {
-    where: String,
-    when: Date
+  userInfo : {
+    rating: Number,
+    tags: [String],
+    review: {
+      public: Boolean,
+      text: String,
+      date: Date,
+      rating: Number
+    },
+    watchedInfo : {
+      where: String,
+      when: Date
+    }
+  },
+  imdbInfo : {
+    title: String,
+    rated: String,
+    released: Date,
+    runtime: String,
+    genres: [String],
+    director: String,
+    actors: String,
+    plot: String,
+    poster: String,
+    rating: String,
+    id: String
   }
 });
 
